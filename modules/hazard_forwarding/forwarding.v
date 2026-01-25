6bb469d259b953f6d8f72a7773010af170327937
ARD-alt <adhvaithrd.mi6@yahoo.com>
2026-01-25 19:03:10 +0530

    comments for clarity

--- modules/hazard_forwarding/forwarding.v
+++ modules/hazard_forwarding/forwarding.v
@@ -9,18 +9,20 @@ module forwarding_EXE (src1_EXE, src2_EXE, ST_src_EXE, dest_MEM, dest_WB, WB_EN_
   always @ ( * ) begin
     // initializing sel signals to 0
     // they will change to enable forwrding if needed.
+    // the lw, add hazard path requires a stall, hence a freeze is implemented in hazardDetection module
     {val1_sel, val2_sel, ST_val_sel} <= 0;
 
     // determining forwarding control signal for store value (ST_val)
-    if (WB_EN_MEM && ST_src_EXE == dest_MEM) ST_val_sel <= 2'd1;
-    else if (WB_EN_WB && ST_src_EXE == dest_WB) ST_val_sel <= 2'd2;
+    if (WB_EN_MEM && ST_src_EXE == dest_MEM) ST_val_sel <= 2'd1; //add, sw
+    else if (WB_EN_WB && ST_src_EXE == dest_WB) ST_val_sel <= 2'd2; //lw, sw
 
     // determining forwarding control signal for ALU val1
-    if (WB_EN_MEM && src1_EXE == dest_MEM) val1_sel <= 2'd1;
-    else if (WB_EN_WB && src1_EXE == dest_WB) val1_sel <= 2'd2;
+    if (WB_EN_MEM && src1_EXE == dest_MEM) val1_sel <= 2'd1; //add, add
+    else if (WB_EN_WB && src1_EXE == dest_WB) val1_sel <= 2'd2; //lw, add
 
     // determining forwarding control signal for ALU val2
-    if (WB_EN_MEM && src2_EXE == dest_MEM) val2_sel <= 2'd1;
-    else if (WB_EN_WB && src2_EXE == dest_WB) val2_sel <= 2'd2;
+    if (WB_EN_MEM && src2_EXE == dest_MEM) val2_sel <= 2'd1;  //add, add
+    else if (WB_EN_WB && src2_EXE == dest_WB) val2_sel <= 2'd2;  //lw, add
   end
 endmodule // forwarding
+
